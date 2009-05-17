1ce001ec31cf65e5ca2ed4926525af8d9426203f
pfreestone <pfreestone@de40e7f4-3aa8-11de-9e80-b52b74bdfc52>
2009-05-17 23:05:47 +0000

    Updating for table structure that include association tables for genre and director/

--- database/table/database_creation_DDL.sql
+++ database/table/database_creation_DDL.sql
@@ -1,8 +1,6 @@
---General Comments/Questions
---Should we use IDs? I prefer this - particularly because the inclusion of
---multivalued attrivutes (like Genre and Director) becomes complicated without
---this mechanism
+--INFO 613 DDL movie data web site
 
+--Actor entity table
 CREATE TABLE actor (
 	actor_id	numeric(10)		NOT NULL,
 	first_name	varchar2(50)	NOT NULL,
@@ -10,8 +8,7 @@ CREATE TABLE actor (
 	CONSTRAINT actor_pk PRIMARY KEY (actor_id)
 );
 
---Should we use a numeric for the year? This data type is not enforced
---in the DTD, so we'd have to ensure it at application level
+--Movie entity table
 CREATE TABLE movie (
 	movie_id	numeric(10)		NOT NULL,
 	title		varchar2(200)	NOT NULL,
@@ -19,6 +16,7 @@ CREATE TABLE movie (
 	CONSTRAINT movie_pk PRIMARY KEY (movie_id)
 );
 
+--Association table between Actor and Movie entities
 CREATE TABLE plays (
 	actor_id	numeric(10)	NOT NULL,
 	movie_id	numeric(10)	NOT NULL,
@@ -27,18 +25,36 @@ CREATE TABLE plays (
 	CONSTRAINT plays_movie_fk FOREIGN KEY (movie_id) REFERENCES movie(movie_id)	
 );
 
-CREATE TABLE movie_director (
-	movie_id	numeric(10)		NOT NULL,
+--Director entity table
+CREATE TABLE director (
+	director_id	numeric(10)		NOT NULL,
 	director	varchar2(100)	NOT NULL,
-	CONSTRAINT movie_director_pk PRIMARY KEY (movie_id,director),
-	CONSTRAINT movie_director_movie_fk FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+	CONSTRAINT director_pk PRIMARY KEY (director_id)
 );
 
+--Genre entity table
+CREATE TABLE genre (
+	genre_id	numeric(10)		NOT NULL,
+	genre		varchar2(100)	NOT NULL,
+	CONSTRAINT genre_pk PRIMARY KEY (genre_id)
+);
+
+--Associative table bewteen Movie and Director entities
+CREATE TABLE movie_director (
+	movie_id	numeric(10)		NOT NULL,
+	director_id	numeric(10)		NOT NULL,
+	CONSTRAINT movie_director_pk PRIMARY KEY (movie_id,director_id),
+	CONSTRAINT movie_director_movie_fk FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+	CONSTRAINT movie_director_director_fk FOREIGN KEY (director_id) REFERENCES director(director_id)
+);
+
+--Associative table between Movie and Genre entities
 CREATE TABLE movie_genre (
 	movie_id	numeric(10)		NOT NULL,
-	genre		varchar2(100)	NOT NULL,
-	CONSTRAINT movie_genre_pk PRIMARY KEY (movie_id,genre),
-	CONSTRAINT movie_genre_movie_fk FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+	genre_id	numeric(10)		NOT NULL,
+	CONSTRAINT movie_genre_pk PRIMARY KEY (movie_id,genre_id),
+	CONSTRAINT movie_genre_movie_fk FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+	CONSTRAINT movie_genre_genre_fk FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
 --The purpose of this table is to provide a centralized location
