77ae3ce0f2c9a0b857a12f32e54f722be3cfea17
pfreestone <pfreestone@de40e7f4-3aa8-11de-9e80-b52b74bdfc52>
2009-05-24 18:04:46 +0000

    Readding scripts.

---
+++ database/table/table_deletion.sql
@@ -0,0 +1,13 @@
+/* Context: INFO 613 DDL movie data web site*/
+/* File Name: DDL Deletion Script*/
+/* Date: May 14, 2009*/
+/* The deletion script shall remove the entities.*/
+
+DROP TABLE sequence;
+DROP TABLE movie_genre;
+DROP TABLE movie_director;
+DROP TABLE genre;
+DROP TABLE director;
+DROP TABLE plays;
+DROP TABLE movie;
+DROP TABLE actor;
---
+++ database/table/table_population.sql
@@ -0,0 +1,34 @@
+/* Context: INFO 613 DDL movie data web site*/
+/* File Name: DDL Population Script*/
+/* Date: May 23, 2009*/
+/* Overview: This script shall populate dummy 
+ * records to test entities, relations, and data types. */
+
+INSERT INTO ACTOR 
+VALUES (1, 'Julia', 'Roberts');
+INSERT INTO MOVIE
+VALUES (1, 'M', '2001');
+INSERT INTO MOVIE 
+VALUES (2, 'Duck Soup', '2004');
+INSERT INTO PLAYS 
+VALUES (1, 1);
+INSERT INTO PLAYS 
+VALUES (1,2);
+INSERT INTO DIRECTOR 
+VALUES (1,'Steven Spielberg');
+INSERT INTO DIRECTOR 
+VALUES (2,'Batuhan Yukselen');
+INSERT INTO GENRE 
+VALUES (1,'Comedy');
+INSERT INTO GENRE 
+VALUES (2,'Musical');
+INSERT INTO GENRE 
+VALUES (3,'(more)');
+INSERT INTO MOVIE_DIRECTOR 
+VALUES (1,1);
+INSERT INTO MOVIE_DIRECTOR 
+VALUES (2,2);
+INSERT INTO MOVIE_GENRE 
+VALUES (1,1);
+INSERT INTO MOVIE_GENRE 
+VALUES (2,2);
