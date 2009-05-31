2705481d19555987e1a6301c2dc40eb85cfe94bf
pfreestone <pfreestone@de40e7f4-3aa8-11de-9e80-b52b74bdfc52>
2009-05-31 19:32:12 +0000

    Added javadocs.

--- trunk/src/edu/drexel/info613/moviedatasite/servlet/TruncateDBServlet.java
+++ trunk/src/edu/drexel/info613/moviedatasite/servlet/TruncateDBServlet.java
@@ -10,6 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * This servlet is responsible for handling requests to truncate the database - a.k.a. delete the data
+ * from the database tables.
+ * 
+ * @author Patrick Freestone, Batuhan Yukselen
+ *
+ */
 public class TruncateDBServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1;
@@ -17,19 +24,41 @@ public class TruncateDBServlet extends HttpServlet {
     private static final String HTML_STREAM_BEGIN = "<html><head><title>Truncate DB Results</title></head><body><p><a href=\"Index.html\">Home Page</a></p>";
     private static final String HTML_STREAM_END   = "</body></html>";
 
+    /**
+     * This is the main method where the behavior is stored. This method basically calls as stored 
+     * procedure in the database that cleans out the tables and then passes the result along to the
+     * <code>writeTruncateStatusPage</code> method which prints out a results HTML page.
+     * 
+     * @param request   HTTP request sent to the servlet
+     * @param response  HTTP response to be sent back to the requester
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
         DBResult truncateResult = MovieDB.getInstance().truncateDB();
         writeTruncateStatusPage(response, truncateResult);
     }
 
+    /**
+     * Method for handling form using the "Get" method. All requests are forwarded to the processRequest
+     * method.
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         processRequest(request, response);
     }
 
+    /**
+     * Method for handling form using the "Post" method. All requests are forwarded to the processRequest
+     * method.
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         processRequest(request, response);
     }
     
+    /**
+     * Method responsible for writing the DB Truncation Result page.
+     * 
+     * @param response          HTTP response to which to write web page
+     * @param truncateResult    result of DB truncation to include in response
+     */
     private void writeTruncateStatusPage(HttpServletResponse response, DBResult truncateResult) {
         try {
             PrintWriter out = response.getWriter();
